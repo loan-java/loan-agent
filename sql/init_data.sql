f4c52fd5bfe617e20c9e7f47cd47a30575cdcba0
shuailj <178397218@qq.com>
2019-06-27 23:34:11 +0800

    sql补全

--- sql/init_data.sql
+++ sql/init_data.sql
@@ -4,10 +4,11 @@ INSERT INTO  `tb_merchant`(`merchant_no`, `merchant_name`, `weight`, `host`, `st
 
 
 -- 插入用户id跟md5值
-select concat('insert into tb_user(id, user_name, mobile_id_md5) values(', t.id, ', "', t.user_name, '",', '"', t.md5,'"', ');') from (select id, user_name, (md5(CONCAT(user_phone,UPPER(user_cert_no)))) as md5 from tb_user) t;
+select concat('insert into tb_user(id, user_name,mobile_no,source, mobile_id_md5,create_time,update_time) values(', t.id, ', "', t.user_name, '",', '"', t.user_phone, '",', '', 1, ',', '"', t.md5, '",', '"', t.create_time, '",', '"', t.update_time,'"', ');') from (select id, user_name, user_phone,(md5(CONCAT(user_phone,UPPER(user_cert_no)))) as md5,now() as create_time,now() as update_time from tb_user) t;
 
 -- 插入order_no跟用户id关联
-select concat('insert into tb_order_user(order_no, user_id) values(', '"', t.order_no, '"', ',', t.uid, ');') from tb_user_order t;
+select concat('insert into tb_order_user(order_no, user_id,source,create_time) values(', '"', t.order_no, '"', ',', t.uid, '', ',', 1 , '', ',"',now() ,'");') from tb_user_order t;
+
 
 -- 插入用户跟商户关联
 select concat('insert into tb_user_merchant(merchant_no, uid) values(', '"10000"', ',', t.id, ');') from tb_user t;
